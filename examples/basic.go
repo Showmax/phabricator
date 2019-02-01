b2b6a594934774df6f46463ec67534846238accc
Ondrej Oprala <ondrej.oprala@showmax.com>
2019-02-01 16:06:19 +0100

    Amend the example

--- examples/basic.go
+++ examples/basic.go
@@ -16,37 +16,40 @@ current use in the past week.
 */
 func main() {
 	var phab phabricator.Phabricator
-	phab.Init("https://phabricator.showmax.cc/api/", "<API_TOKEN>", &phabricator.PhabOptions{
-		LogLevel: "info", // Must be a level recognized by the logrus library
+	tok := "<API-TOKEN>"
+	phab.Init("https://phabricator.showmax.cc/api/", tok, &phabricator.PhabOptions{
+		LogLevel: "error", // Must be a level recognized by the logrus library
 		Timeout:  10 * time.Second,
 	})
 
-	/* Create constraints for the search:
-	 */
+	// Create constraints for the search:
 	ticketArgs := phabTypes.TicketSearchArgs{}
-
 	// Include the PHIDs of people watching the tickets in the results
 	ticketArgs.Attachments.Subscribers = true
 	// Only search the tickets the current user has authored
 	ticketArgs.QueryKey = "authored"
-	// Only consider tickets created in the past week in the search
+	// Only consider tickets created in the past 30 days in the search
 	now := time.Now()
-	sevenDaysAgo := now.AddDate(0, 0, -7)
+	sevenDaysAgo := now.AddDate(0, 0, -30)
 	ticketArgs.Constraints.CreatedStart = sevenDaysAgo.Unix()
 
+	// The context allows you to cancel the current call prematurely
 	ctx, cancelCtx := context.WithCancel(context.Background())
-	tickets, errors := phab.Call(ctx, "maniphest.search", ticketArgs, phabTypes.Ticket{})
-	if tickets == nil || errors == nil {
+	defer cancelCtx()
+	results := phab.Call(ctx, "maniphest.search", ticketArgs, phabTypes.Ticket{})
+	if results == nil {
 		log.Fatal("Non-existent endpoint")
 	}
-	for e := range errors {
-		log.Print(e)
-		cancelCtx()
-		return
+	for t := range results {
+		switch t.(type) {
+		case error:
+			log.Print(t)
+			return
+		case *phabTypes.Ticket:
+			ticket := t.(*phabTypes.Ticket)
+			fmt.Printf("T%d: %s\n", ticket.Id, ticket.Fields.Name)
+		default:
+			log.Panic("Unexpected type received from the Phabricator library")
+		}
 	}
-	for t := range tickets {
-		ticket := t.(*phabTypes.Ticket)
-		fmt.Printf("T%d: %s\n", ticket.Id, ticket.Fields.Name)
-	}
-	cancelCtx()
 }
