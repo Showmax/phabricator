3dbce4d574e096748428919e2c9a62e14a7a552a
Ondrej Oprala <ondrej.oprala@showmax.com>
2019-02-10 15:01:01 +0100

    Improve the example

--- examples/basic.go
+++ examples/basic.go
@@ -17,21 +17,21 @@ current use in the past week.
 func main() {
 	var phab phabricator.Phabricator
 	tok := "<API-TOKEN>"
-	phab.Init("https://phabricator.showmax.cc/api/", tok, &phabricator.PhabOptions{
+	err := phab.Init("https://phabricator.showmax.cc/api/", tok, &phabricator.PhabOptions{
 		LogLevel: "error", // Must be a level recognized by the logrus library
 		Timeout:  10 * time.Second,
 	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Create constraints for the search:
-	ticketArgs := phabTypes.TicketSearchArgs{}
+	ticketArgs := phabTypes.TicketSearchArgs{QueryKey: "authored"}
 	// Include the PHIDs of people watching the tickets in the results
 	ticketArgs.Attachments.Subscribers = true
-	// Only search the tickets the current user has authored
-	ticketArgs.QueryKey = "authored"
 	// Only consider tickets created in the past 30 days in the search
-	now := time.Now()
-	sevenDaysAgo := now.AddDate(0, 0, -30)
-	ticketArgs.Constraints.CreatedStart = sevenDaysAgo.Unix()
+	thirtyDaysAgo := time.Now().UTC().AddDate(0, 0, -30).Unix()
+	ticketArgs.Constraints.CreatedStart = thirtyDaysAgo
 
 	// The context allows you to cancel the current call prematurely
 	ctx, cancelCtx := context.WithCancel(context.Background())
