4f92376cf6e211bd9f76306cbb363cb766222575
Ondrej Oprala <ondrej.oprala@showmax.com>
2019-02-12 17:08:53 +0100

    Revert a dumb fix, add more logging

--- transaction.go
+++ transaction.go
@@ -3,6 +3,8 @@ package phabricator
 import (
 	"encoding/json"
 	"fmt"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type PhabTransaction struct {
@@ -11,11 +13,20 @@ type PhabTransaction struct {
 }
 
 func (pt *PhabTransaction) MarshalJSON() ([]byte, error) {
+
+	logger := logger.WithFields(log.Fields{
+		"tx_type":    pt.Type,
+		"value_type": fmt.Sprintf("%T", pt.Value),
+	})
+
 	val, err := json.Marshal(pt.Value)
 	if err != nil {
+		logger.Error("Failed to marshal a transaction value to JSON")
 		return []byte{}, err
 	}
-	return []byte(fmt.Sprintf(`{ "type": %s, "value": %v }`, pt.Type, val)), nil
+	j := fmt.Sprintf(`{ "type": %s, "value": %s }`, pt.Type, string(val))
+	logger.WithField("marshal", j).Debug("Marshaled a transaction to JSON")
+	return []byte(j), nil
 }
 
 func NewTransaction(tx string, val interface{}) PhabTransaction {
